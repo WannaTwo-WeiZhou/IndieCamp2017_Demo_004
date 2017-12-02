5ca200d2715e8f058f4521922e42f5e976aed59c
WannaTwo <33625108+WannaTwo-WeiZhou@users.noreply.github.com>
2017-12-02 10:45:32 +0800

    click and move

---
+++ Assets/Scripts/ClickObject.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ClickedType
+{
+    NULL = -1,
+
+    OnlyMove = 0,
+    MoveToPreScene = 1,
+    MoveToNextScene = 2,
+    MoveAndSpeak = 3,
+
+    SIZE
+
+};
+
+public class ClickObject : MonoBehaviour
+{
+    public ClickedType m_ClickedType = ClickedType.NULL;
+
+	private Hero m_Hero;
+
+	void Awake()
+	{
+		m_Hero = Global.instance.m_Hero;
+	}
+
+    public void BeClicked(Vector2 pos)
+    {
+        Debug.Log(gameObject.name + "be clicked!!! Type = " + m_ClickedType);
+
+        switch (m_ClickedType)
+        {
+            case ClickedType.OnlyMove:
+                {
+					m_Hero.Move(pos);
+                }
+                break;
+
+            case ClickedType.MoveToPreScene:
+                {
+					m_Hero.Move(pos);
+                }
+                break;
+
+            case ClickedType.MoveToNextScene:
+                {
+					m_Hero.Move(pos);
+                }
+                break;
+
+            case ClickedType.MoveAndSpeak:
+                {
+					m_Hero.Move(pos);
+                }
+                break;
+
+            default:
+                break;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Global.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Global : MonoBehaviour {
+
+	public static Global instance { get; private set; }
+
+	public Hero m_Hero;
+
+	void OnEnable()
+	{
+		if (Global.instance == null)
+		{
+			Global.instance = this;
+		}
+		else if (Global.instance != this)
+		{
+			Destroy(gameObject);
+		}
+	}
+
+
+}
