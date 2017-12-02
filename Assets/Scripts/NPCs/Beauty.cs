5d0f1a80b09e5feb6ae02a2790c375cd3582e659
WannaTwo <33625108+WannaTwo-WeiZhou@users.noreply.github.com>
2017-12-02 21:41:32 +0800

    override speakers

---
+++ Assets/Scripts/NPCs/Beauty.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Beauty : Speaker
+{
+	public override void ShowText()
+	{
+		base.ShowText();
+		
+	}
+
+	public override void LineComplete()
+	{
+		base.LineComplete();
+	}
+}
---
+++ Assets/Scripts/NPCs/SpeakerManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeakerManager : MonoBehaviour {
+
+	public static SpeakerManager instance { get; private set; }
+
+	public Speaker m_ZhenZi;
+	public Speaker m_CryingOldWoman;
+	public Speaker m_Conductor;
+	public Speaker m_Fatty;
+	public Speaker m_Thin;
+	public Speaker m_Handsome;
+	public Speaker m_Beauty;
+	public Speaker m_Mum;
+
+	void OnEnable()
+	{
+		if (SpeakerManager.instance == null)
+		{
+			SpeakerManager.instance = this;
+		}
+		else if (SpeakerManager.instance != this)
+		{
+			Destroy(gameObject);
+		}
+	}
+
+}
