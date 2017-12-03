d9a3d6bb34f07699ef294795b8fefadac565a9f5
24967 <forestlii@outlook.com>
2017-12-03 13:41:55 +0800

    加入了音效

--- Assets/Scripts/Extension/Constants.cs
+++ Assets/Scripts/Extension/Constants.cs
@@ -16,6 +16,18 @@
     public const string BGM_Theme = "Theme";
     public const string BGM_BeyondWorld = "BeyondWorld";
 
+    public const string Noise_Eat = "嚼薯片";
+    public const string Noise_Metre = "地铁呼啸声";
+    public const string Noise_ChildSmile = "天真笑声";
+    public const string Noise_Ghost = "女鬼尖啸";
+    public const string Noise_Hit = "拳头击打椅子背";
+    public const string Noise_FallDown = "摔倒声";
+    public const string Noise_Shine = "珠宝闪亮声";
+    public const string Noise_Light = "电灯电流声";
+    public const string Noise_BeautifulCry = "美女哭泣声";
+    public const string Noise_OldWomenCry = "老妇哭泣声";
+    public const string Noise_DingDing = "铃铛声";
+
     
 
    
